dd650737c090fc910de286664c6161c82dae236e
Randolfo Castillo <castillo.randolfo2@gmail.com>
2021-01-14 12:19:54 -0300

    Add: Solution

---
+++ hackerrank/java/practice-1/src/com/selfcode/Solution.java
@@ -0,0 +1,9 @@
+package com.selfcode;
+
+public class Solution{
+    public static void main(String[] args){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+}
