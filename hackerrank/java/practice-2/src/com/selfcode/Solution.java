206e3c2d638920b99d6cbed222d34973a960e72d
Randolfo Castillo <castillo.randolfo2@gmail.com>
2021-01-14 12:40:12 -0300

    Add: Solution

---
+++ hackerrank/java/practice-2/src/com/selfcode/Solution.java
@@ -0,0 +1,18 @@
+package com.selfcode;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner((System.in));
+
+        ArrayList<Integer> n = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            n.add(scan.nextInt());
+        }
+
+        n.forEach(System.out::println);
+    }
+}
