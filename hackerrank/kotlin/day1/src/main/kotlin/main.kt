2f99e61eb6c4470ac78357a547b5247bd6dee7dc
Randolfo Castillo <castillo.randolfo2@gmail.com>
2021-01-15 15:02:50 -0300

    Add: InputStream

---
+++ hackerrank/kotlin/day1/src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val inputString: String = scan.nextLine()
+    scan.close()
+    println("Hello, World.")
+    println(inputString)
+}
